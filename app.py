c985688791ff7d4ff786c79cd51e3f0bfce0dadf
sieun-oo <ididsep@gmail.com>
2024-10-28 12:22:53 +0900

    네이버

--- app.py
+++ app.py
@@ -4,21 +4,21 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
-    
-@app.route('/soccer')
-def soccer():
-    return 'Hello, soccer!'
 
-    @app.route('/naver')
-def soccer():
-    return 'render_template('naver.html')'
+@app.route('/baseball')
+def baseball():
+    return 'Hello, baseball!'
 
-    @app.route('/method', methods=['GET', 'POST'])
+@app.route('/naver')
+def naver():
+    return render_template('naver.html')
+
+@app.route('/method', methods=['GET', 'POST'])
 def method():
     if request.method == 'GET':
         return "GET으로 전달"
     else:
-        keyworld = request.form["keyword"]
+        keyword = request.form["keyword"]
         print(keyword)
         return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
 
