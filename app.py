b8ab93c0d4d2cdcc8094c56a407164f033cc6f38
sieun-oo <ididsep@gmail.com>
2024-11-04 12:26:42 +0900

    입력한 이름으로 유툽 검색 따라하기

--- app.py
+++ app.py
@@ -3,15 +3,34 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World! 기말 우뜨콰라고'
 
-@app.route('/baseball')
-def baseball():
-    return 'Hello, baseball!'
+@app.route('/soccer')
+def soccer():
+    return 'Hello, soccer!'
 
 @app.route('/naver')
 def naver():
     return render_template('naver.html')
+
+@app.route('/youtube', methods=['GET', 'POST'])
+def youtube():
+    linklist = ["https://www.youtube.com/embed/FhLD58QnRM4?si=XFUJjxIy2k3__1KV","https://www.youtube.com/embed/GnSmlzpiHVE?si=p4jt7jLs_Lfl-39R"]
+    keyword = request.form["keyword"]
+    print(keyword)
+    linknum = 0 # 0은 호피폴라, 1은 하현상
+    if keyword == '호피폴라':
+        linknum = 0
+    elif keyword == '하현상':
+        linknum = 1
+    else:
+        print('잘못된 입력')
+
+    return render_template('youtube.html', name=keyword, link=linklist[linknum])
+
+@app.route('/ytpage')
+def ytpage():
+    return render_template('ytpage.html')
 
 @app.route('/method', methods=['GET', 'POST'])
 def method():
@@ -23,4 +42,4 @@ def method():
         return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
 
 if __name__ == '__main__':
-    app.run( )
+    app.run(host="0.0.0.0")
